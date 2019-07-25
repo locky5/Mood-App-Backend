94763a02e5e510d6fe6a646d10f8dcd5e50d76b6
locky5 <daikevin19@yahoo.com>
2019-07-25 14:16:48 -0400

    added models and controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post1 = Post.new(description: "it's a live one")
+post2 = Post.new(description: "hi")
+post3 = Post.new(description: "oioioi")
+post4 = Post.new(description: "nonono")
+post5 = Post.new(description: "okay")
