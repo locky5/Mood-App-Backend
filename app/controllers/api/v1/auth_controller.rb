a7ae9181d2eb6243ed1e7d5fce8fc98a900e0e03
Aaron Smith <smitham50@gmail.com>
2019-07-29 12:57:22 -0400

    fixed login 500 error

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::AuthController < ApplicationController
 
   def login
-    user = User.find_by(username: params[:name])
+    user = User.find_by(name: params[:name])
 
     if user && user.authenticate(params[:password])
-        token = encode_token(user.id)
-      render json: {user: user, token: token}
+        # token = encode_token(user.id)
+      render json: user
     else
-        render json: {errors: "Login Failed"}
+      render json: {errors: "Login Failed"}
     end
   end
 
